f9032284fa8bc7f0aa85795ecb3e1835fedc1e0b
Vishwanath Hiremath <vishwah1312@gmail.com>
2024-07-17 14:54:57 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ resource "aws_vpc" "test_vpc" {
 resource "aws_subnet" "test_subnet" {
   vpc_id            = aws_vpc.test_vpc.id
   cidr_block        = "10.0.0.0/24"
-  availability_zone = "us-east-2a"
+  availability_zone = "us-west-2a"
 }
 
 resource "aws_memorydb_subnet_group" "test_subnet_group" {
